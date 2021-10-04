6a419e7d055e1dc556685af46d180f496b7a2b13
Vaibhav Karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2021-10-05 02:19:25 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {AiFillLinkedin} from "react-icons/ai";
-import {AiOutlineGithub} from "react-icons/ai";
-import {FiTwitter} from "react-icons/fi";
-import {AiOutlineInstagram} from "react-icons/ai";
+import github from "../Assets/github.png";
+import instagram from "../Assets/instagram.png";
+import twitter from "../Assets/twitter.png";
+import linkedin from "../Assets/linkedin.png";
 
 function Footer() {
   let date = new Date();
@@ -18,48 +18,44 @@ function Footer() {
           <h3>Copyright © {year} itsmevaibhavkarnwal</h3>
         </Col>
         <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
+            <ul className="footer-icons">
+              <li className="social-icons">
                 <a
                   href="https://github.com/vaibhav-karnwal"
-                  style={{ color: "white" }}
                   target="_blank"
-                  rel="noopener noreferrer"
-                  className="icon-colour"
-                  ><AiOutlineGithub/>
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  ><img src={github} className="icon-size " ></img>
                 </a>
               </li>
               <li className="social-icons">
                 <a
                   href="https://twitter.com/algocreater"
-                  style={{ color: "white" }}
                   target="_blank"
-                  rel="noopener noreferrer"
-                  className="icon-colour  "
-                  ><FiTwitter/>
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  ><img src={twitter} className="icon-size " ></img>
                 </a>
               </li>
               <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/vaibhav-karnwal/"
-                  style={{ color: "white" }}
                   target="_blank"
-                  rel="noopener noreferrer"
-                  className="icon-colour "
-                  ><AiFillLinkedin/>
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  ><img src={linkedin} className="icon-size " ></img>
                 </a>
               </li>
               <li className="social-icons">
                 <a
                   href="https://www.instagram.com/vaibhavkarnwal2812/"
-                  style={{ color: "white" }}
                   target="_blank"
-                  rel="noopener noreferrer"
-                  className="icon-colour"
-                  ><AiOutlineInstagram/>
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                  ><img src={instagram} className="icon-size" ></img>
                 </a>
               </li>
-          </ul>
+            </ul>
         </Col>
       </Row>
     </Container>
