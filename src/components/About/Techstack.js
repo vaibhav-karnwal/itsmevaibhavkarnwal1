a601b794d450d594039267f2fb1203338b61d2fa
Vaibhav Karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2021-10-04 16:09:26 +0530

    Add files via upload

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,38 +1,49 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import {RiReactjsFill} from "react-icons/ri";
+import {RiFlutterFill} from "react-icons/ri";
+import {FaJava} from "react-icons/fa";
+import {DiJavascript1} from "react-icons/di";
+import {AiFillHtml5} from "react-icons/ai";
+import {FaCss3} from "react-icons/fa";
+import {DiBootstrap} from "react-icons/di";
+import {FaSass} from "react-icons/fa";
+import {AiFillGithub} from "react-icons/ai";
+import {FaNodeJs} from "react-icons/fa";
+
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <RiReactjsFill/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <RiFlutterFill/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <FaJava/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <DiJavascript1/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-       
+        <AiFillHtml5/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <FaNodeJs/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-       
+        <FaSass/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <DiBootstrap/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <AiFillGithub/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        
+        <FaCss3/>
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import {DiAndroid} from "react-icons/di";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-      
+      <DiAndroid/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-    
+      
       </Col>
       <Col xs={4} md={2} className="tech-icons">
    
       </Col>
       <Col xs={4} md={2} className="tech-icons">
- 
+      
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-  
+      
       </Col>
     </Row>
   );
